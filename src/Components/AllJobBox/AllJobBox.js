f9c63c6c575330462785a7f3f2994892fbc847a0
shadreza <45334134+shadreza@users.noreply.github.com>
2021-06-20 16:48:22 +0600

    Have implemented a card

---
+++ src/Components/AllJobBox/AllJobBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AllJobBox.css';
+
+const AllJobBox = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AllJobBox;
