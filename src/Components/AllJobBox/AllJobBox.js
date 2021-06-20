8a5b7916bc6ba2fa2c10e9388e9bf0b3eb79dcd9
shadreza <45334134+shadreza@users.noreply.github.com>
2021-06-21 02:51:23 +0600

    Tweaked the HomePage and the cards and such... The backend has been connected via local host and now will try to add some jobs via

--- src/Components/AllJobBox/AllJobBox.js
+++ src/Components/AllJobBox/AllJobBox.js
@@ -9,7 +9,7 @@ const AllJobBox = () => {
 
     const allJobs = useContext(AllJobsContext);
     let array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68];
-    let oneCard = [image , "Front End Junior Web Developer", "Internship", "This is the descriptionThis is the descriptionThis is the descriptionThis is the descriptionThis is the descriptionThis is the descriptionThis is the descriptionThis is the descriptionThis is the descriptionThis is the descriptionThis is the description", 1500.00, "1 years Experience", "Remote", ["Add","Remove"]];
+    let oneCard = [image , "Front End Junior Web Developer", "Internship", "This is the descriptionThis is the descriptionThis is the descriptionThis is the descriptionThis is the descriptionThis is the descriptionThis is the descriptionThis is the descriptionThis is the descriptionThis is the descriptionThis is the description", 1500.00, "1 years Experience", "Remote", ["Go For It!","Learn More"]];
     const [howManyCardsInOnePage , setHowManyCardsInOnePage] = useState(20);
     let newArray = array;
     const [totalPages , setTotalPages] = useState(Math.ceil((array.length)/howManyCardsInOnePage))
